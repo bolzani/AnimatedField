8c468348bf707ebe3b4c7627e722e7887aa07ab6
Felipe Alves <felipe@bolzanialves.com.br>
2019-08-31 12:43:51 -0300

    Criando tipo Custom

--- AnimatedField/Classes/AnimatedFieldType.swift
+++ AnimatedField/Classes/AnimatedFieldType.swift
@@ -23,6 +23,7 @@ public enum AnimatedFieldType {
     case name
     case phone
     case custompicker([String], String?) // options, choose text
+    case custom(typingExpression: String?, validationExpression: String?, format: String?, validationError: String)
     
     var decimal: String {
         var separator = Locale.current.decimalSeparator ?? "\\."
@@ -35,6 +36,7 @@ public enum AnimatedFieldType {
         case .email: return "[A-Z0-9a-z@_\\.]"
         case .username: return "[A-Za-z0-9_.]"
         case .price: return "[0-9\(decimal)]"
+        case .custom(.some(let exp), _, _, _): return exp
         default: return ".*"
         }
     }
@@ -48,20 +50,17 @@ public enum AnimatedFieldType {
         case .url: return "https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,}"
         case .cpf: return "([0-9]{2}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[\\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\\.]?[0-9]{3}[\\.]?[0-9]{3}[-]?[0-9]{2})"
         case .phone: return "(?:(?:\\+|00)?(55)\\s?)?(?:\\(?([1-9][0-9])\\)?\\s?)?(?:((?:9\\d|[2-9])\\d{3})\\-?(\\d{4}))"
+        case .custom(_, .some(let exp), _, _): return exp
         default: return ".*"
         }
     }
     
     var validationError: String {
         switch self {
-        case .email: return "E-mail inválido!"
         case .username(let min, let max): return "Nome de usuário deve ter entre \(min) e \(max) caracteres!"
         case .password(let min, let max): return "A senha deve ter entre \(min) e \(max) caracteres!"
-        case .price: return "Preço inválido!"
-        case .url: return "Url inválida!"
-        case .cpf: return "CPF inválido!"
-        case .phone: return "Telefone inválido!"
-        default: return ""
+        case .custom(_, _, _, let error): return error
+        default: return "Valor inválido!"
         }
     }
     
@@ -76,6 +75,7 @@ public enum AnimatedFieldType {
         switch self {
         case .cpf: return "###.###.###-##"
         case .phone: return "(##) ####-####"
+        case .custom(_, _, .some(let format), _): return format
         default: return nil
         }
     }
